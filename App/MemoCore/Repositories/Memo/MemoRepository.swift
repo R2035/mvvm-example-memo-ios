9611f276bc34d4ae769ecd185b2a16aa242becef
R2035 <r2035.develop@gmail.com>
2021-10-16 14:44:51 +0900

    DIを実装

---
+++ App/MemoCore/Repositories/Memo/MemoRepository.swift
@@ -0,0 +1,11 @@
+//
+//  MemoRepository.swift
+//  MemoCore
+//
+//  Created by oe on 2021/10/16.
+//
+
+import Foundation
+
+/// MemoのRepository
+public protocol MemoRepository {}
---
+++ App/MemoCore/Repositories/Memo/RealmMemoRepository.swift
@@ -0,0 +1,11 @@
+//
+//  RealmMemoRepository.swift
+//  MemoCore
+//
+//  Created by oe on 2021/10/16.
+//
+
+import Foundation
+
+/// Realmを使ったMemoRepositoryの実装
+final class RealmMemoRepository: MemoRepository {}
